eae8a759bfa03f8c4f794b807cd09ed216f67010
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2017-03-14 14:44:04 +0100

    Added Gui Code
    Yeah pretty much just this

--- src/tests/testGUI.java
+++ src/tests/testGUI.java
@@ -1,5 +1,17 @@
 package tests;
 
+import org.junit.Test;
+
+import game.GameGUIImplementation;
+
 public class testGUI {
 
+	@Test
+	public void testGui(){
+		new GameGUIImplementation();
+		while(true){
+			
+		}
+	}
+	
 }
