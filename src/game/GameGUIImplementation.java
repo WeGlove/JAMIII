4ffc6bc5d947913d96cc44b8703658a57e852208
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2017-03-14 13:44:46 +0100

    Added test gui class

--- src/game/GameGUIImplementation.java
+++ src/game/GameGUIImplementation.java
@@ -1,8 +1,20 @@
 package game;
 
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
 public class GameGUIImplementation implements GameGUI {
 
+	JFrame Frame;
+	BufferedImage BImg;
+	
 	public GameGUIImplementation(){
+		
+		Frame = new JFrame();
+		Frame.setBounds(10, 10, 10, 10);
+		Frame.setVisible(true);
+		
 	}
 	
 }
